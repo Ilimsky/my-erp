92d4a3747f1c4b19efd744f7c74fa81fbbe54b07
Ilimsky <ilim.sarykbaev@gmail.com>
2024-07-25 16:09:27 +0600

    Modified some files

--- employee-service/src/main/resources/db.migration/V3__create_employee_table.sql
+++
@@ -1,9 +0,0 @@
-//V3__create_employee_table.sql
-
-CREATE TABLE employee
-(
-    employee_id     BIGINT AUTO_INCREMENT PRIMARY KEY,
-    employee_name   VARCHAR(255) NOT NULL,
-    department_id   BIGINT,
-    department_name VARCHAR(255)
-);
