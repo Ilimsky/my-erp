f80c488c0f9f37f98c6aa70a06d954016fa40477
Ilimsky <ilim.sarykbaev@gmail.com>
2024-07-26 17:23:35 +0600

    Setup connection between config-repo and services

---
+++ internalOrder-service/src/main/java/com/example/internalorderservice/InternalOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.internalorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternalOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternalOrderServiceApplication.class, args);
+    }
+
+}
---
+++ item-servcie/src/main/java/com/example/itemservcie/ItemServcieApplication.java
@@ -0,0 +1,13 @@
+package com.example.itemservcie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemServcieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemServcieApplication.class, args);
+    }
+
+}
